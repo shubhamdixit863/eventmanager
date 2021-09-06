fe713a1eb4c4f60b881d25f89d7a3934023b808e
shubham dixit <shubhamdixit863@gmail.com>
2021-09-06 23:40:55 +0530

    initailized

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Shiftlist from './components/Shiftlist';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+      <Header/>
+
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/login">
+            <Login/>
+          </Route>
+
+
+          <Route path="/signup">
+            <Signup/>
+          </Route>
+         
+
+          <Route path="/shiftlist">
+            <Shiftlist/>
+          </Route>
+        </Switch>
+     
+    </Router>
+      
     </div>
   );
 }
---
+++ src/components/CreateShift.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CreateShift() {
+    return (
+        <div className="w-full max-w-xs">
+       
+        <p className="text-center text-gray-500 text-xs">
+          &copy;2020 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    )
+}
+
+export default CreateShift
